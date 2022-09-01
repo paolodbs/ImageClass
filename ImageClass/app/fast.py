6decdd724f645b3a0a46d6bbae54d322559972bc
Nicole Chantzi <nicolechantzi@gmail.com>
2022-09-02 01:45:58 +0300

    fixed fast api & added a test requests file

--- ImageClass/app/fast.py
+++ ImageClass/app/fast.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , UploadFile , File
+from fastapi import FastAPI , UploadFile , File , Request
 import requests
 # ATTENTION not sure of that:
 from ImageClass.architecture import initialize_model_3
@@ -9,23 +9,27 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 import numpy as np
 import cv2
+import io
+from typing import List
+
 app = FastAPI()
+
 #ATTENTION not very sure of that:
-app.state.model = load_model('../../notebooks/my_model')
+#app.state.model = load_model('../../notebooks/my_model')
+
+##store model to cache mem
+app.state.model = load_model('../../raw_data/my_lovely_model')
+
+##store categories to cache mem
 app.state.labels = create_class_dic()
 
 @app.get("/")
-def index():
-    return {"Welcome to our Image Predictor!": True}
+def home():
+    return {"Message": "Welcome to our Image Predictor!"}
 
 #as a reminder now we can run this code in our command line: uvicorn simple:app --reload
 
-url = 'http://localhost:8000/predict'
-
 #ATTENTION NOT FINISHED!!!!!!!!:
-params = {
-    'image': "dunno lol"
-}
 
 #response = requests.get(url, params=params)
 #response.json()
@@ -41,14 +45,72 @@ params = {
 #    category = categories.get(y_pred)
 #    return {"Here you have the prediction": category}
 
-def predict(image):
-    X_pred = cv2.imread(image.file.read())
-    X_pred = cv2.resize(X_pred , (32,32))
-    prediction = app.state.model.predict(X_pred)
-    return prediction
-
 @app.post('/predict')
-def make_prediction(image : UploadFile = File(...)):
-    prediction = predict(image)
+async def make_prediction(image : UploadFile = File(...)):
+    """Returns the prediction of a given image:
+    /TESTED against jpgs
+    """
+    ##!after plently of debugging a clean solution!
+
+    ##returns the bytes encoding of the image
+    request_object_content = await image.read()
+
+    ##creates a PIL object and converts it to RGB
+    ##bear in mind PIL cannot directly read the byte string
+    ##Hence, we need to save it to the buffer and convert it to RGB (RGB may be by default)
+    img = Image.open(io.BytesIO(request_object_content)).convert("RGB")
+
+    ## PIL stores images as 32-bits floats by default / No need to resize
+    ## - however with a different model architecture
+    ## the reshape below might be required
+    img_reshaped = cv2.resize(np.array(img) , (32,32))
+
+    ##may need to normalize for better predictions
+    ##TODO : uncomment me if u want
+    ##img_reshaped = img_reshaped/255
+
+    ##againt np.array([ . ]) expands the dimension by 1 in front ~ required by tensorflow
+    prediction =  app.state.model.predict( np.array([img_reshaped]) )
+
+    ##finally predict the category
+    ##prediction returns the array of predicted probabilities belonging to each particular class
+    ##i.e. model doesn't predict the class but the probabilities of belonging to each particular class
+    ##Finally, we choose the class corresponding to the highest predicted probability
+    ##and we return the category corresponding to the index with the highest probability
     predicted_category = app.state.labels.get( np.argmax(prediction[0]) )
-    return {'prediction':predicted_category}
+
+    ##sending the server response / if status_code == 200
+    return {'prediction' : predicted_category}
+
+
+def preprocess_img(object_content):
+    """Basic image preprocessing"""
+    img = Image.open(io.BytesIO(object_content)).convert("RGB")
+    img_reshaped = cv2.resize(np.array(img) , (32,32))
+    img_normalized = img_reshaped/255
+    return img_normalized
+
+
+@app.post('/multipredict')
+async def get_multiple_predictions(files : List[UploadFile] = File(...)):
+    """Receives a collection of images and returns the dictionary
+    of predicted categories for each image"""
+    #TODO ~ in debugging phase
+    content = { image.name : preprocess_img(await image.read()) for image in files }
+    predictions = dict()
+    for name , img in content.items():
+        prediction =  app.state.model.predict( np.array([img]) )
+        predicted_category = app.state.labels.get( np.argmax(prediction[0]) )
+        predictions.update({name : predicted_category })
+    return {"predictions": predictions }
+
+
+@app.post('/filename')
+async def pred(image : UploadFile = File(...)):
+    """TEST endpoint
+                Returns image filename
+    /TESTED against pngs"""
+
+    request_object_content = await image.read()
+    img = Image.open(io.BytesIO(request_object_content))
+    return {"prediction" : image.filename}
---
+++ ImageClass/app/test.py
@@ -0,0 +1,32 @@
+import requests
+from pathlib import Path
+
+#PATH NEEDS TO CHANGE
+#SOLELY FOR TESTING PURPOSES
+path_to_images = "/home/nicole/code/NicoleChant/images"
+local_api_url = "http://127.0.0.1:8000/multipredict"
+
+def get_extension(filename : str) -> str:
+    return filename.split(".")[-1] if "." in filename else filename
+
+def get_predictions(directory : Path) -> list[str]:
+    images = []
+    print("Preparing request...")
+    admissible_file_extensions = ["jpg","png","jpeg"]
+    for item in directory.iterdir():
+        if item.is_file() and get_extension(item.name) in admissible_file_extensions:
+            images.append( ("file" , open(item , mode = "rb") ) )
+
+    print("Sending Request...")
+    with requests.Session() as session:
+        response = session.post( local_api_url , data = images)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {"error" : response.content }
+
+
+if __name__ == "__main__":
+    directory = Path(path_to_images)
+    predictions = get_predictions(directory)
+    print(predictions)
